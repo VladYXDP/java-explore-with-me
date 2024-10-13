1804e346042e71058ba0e5fa70d11f24e96c263f
Vlad Lysenko <me2ur.cat@yandex.ru>
2024-10-13 15:56:08 +0300

    Прохождение тестов 19

--- ewm-service/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
